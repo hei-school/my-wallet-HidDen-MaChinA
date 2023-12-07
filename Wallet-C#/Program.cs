6d0d7d7364beec7546dc82c7bcc0c80e0c255809
HidDen-MaChinA <hei.franco.3@gmail.com>
2023-12-07 13:34:59 +0100

    feat:C# implementation

--- Wallet-C#/Program.cs
+++ Wallet-C#/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using Wallet;
+public class WalletMain
+{
+    private static WalletConsoleInterface interfaces = new();
+    public static void Main(String[] args)
+    {
+        interfaces.InitializeWallet();
+        while (true)
+        {
+            switch (interfaces.MainMenu())
+            {
+                case 0:
+                    interfaces.History();
+                    return;
+                case 1:
+                    interfaces.SpendMoneyInterface();
+                    continue;
+                case 2:
+                    interfaces.AddMoneyInterface();
+                    continue;
+                case 3:
+                    interfaces.ManipulateWalletBudgets();
+                    continue;
+                case 4:
+                    interfaces.History();
+                    continue;
+            }
+        }
+    }
+}
